fc0cd9782dc587c008087812e27c33fe0914e62a
Cornelis van der Bent <cornelis@meaning-matters.com>
2019-01-23 11:53:51 +0100

    Add OpenSSL sources and make it build

--- openssl/openssl.h
+++ openssl/openssl.h
@@ -14,6 +14,37 @@ FOUNDATION_EXPORT double opensslVersionNumber;
 //! Project version string for openssl.
 FOUNDATION_EXPORT const unsigned char opensslVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <openssl/PublicHeader.h>
+#define OPENSSL_NO_SEED
+#define OPENSSL_NO_IDEA
+#define OPENSSL_NO_RC2
+#define OPENSSL_NO_RC4
+#define OPENSSL_NO_DES
+#define OPENSSL_NO_SM4
+#define OPENSSL_NO_BF
+#define OPENSSL_NO_CHACHA
 
-
+#import <openssl/opensslconf.h>
+#import <openssl/bn.h>
+#import <openssl/ec.h>
+#import <openssl/obj_mac.h>
+#import <openssl/e_os2.h>
+#import <openssl/ossl_typ.h>
+#import <openssl/crypto.h>
+#import <openssl/safestack.h>
+#import <openssl/opensslv.h>
+#import <openssl/cryptoerr.h>
+#import <openssl/symhacks.h>
+#import <openssl/bnerr.h>
+#import <openssl/asn1.h>
+#import <openssl/asn1err.h>
+#import <openssl/ecerr.h>
+#import <openssl/evp.h>
+#import <openssl/bio.h>
+#import <openssl/bioerr.h>
+#import <openssl/evperr.h>
+#import <openssl/objects.h>
+#import <openssl/objectserr.h>
+#import <openssl/rand.h>
+#import <openssl/randerr.h>
+#import <openssl/ripemd.h>
+#import <openssl/stack.h>
