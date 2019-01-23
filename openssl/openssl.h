32d192ecc5334a1fef077de095c729779f835b2c
Cornelis van der Bent <cornelis@meaning-matters.com>
2019-01-23 11:21:59 +0100

    Initial Commit

---
+++ openssl/openssl.h
@@ -0,0 +1,19 @@
+//
+//  openssl.h
+//  openssl
+//
+//  Created by Cornelis van der Bent on 23/01/2019.
+//  Copyright © 2019 ConsenSys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for openssl.
+FOUNDATION_EXPORT double opensslVersionNumber;
+
+//! Project version string for openssl.
+FOUNDATION_EXPORT const unsigned char opensslVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <openssl/PublicHeader.h>
+
+
---
+++ opensslTests/opensslTests.m
@@ -0,0 +1,37 @@
+//
+//  opensslTests.m
+//  opensslTests
+//
+//  Created by Cornelis van der Bent on 23/01/2019.
+//  Copyright © 2019 ConsenSys. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface opensslTests : XCTestCase
+
+@end
+
+@implementation opensslTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
